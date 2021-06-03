27ec4586a42848f97cd451a1e74e74cfc53218ad
Valeriy Soloviov <weldpua2008@gmail.com>
2021-06-03 22:37:06 +0300

    Update jobs_fetch.go

--- job/jobs_fetch.go
+++ job/jobs_fetch.go
@@ -11,12 +11,6 @@ import (
 	"time"
 )
 
-// ApiJobRequest is struct for new jobs
-type ApiJobRequest struct {
-	JobStatus string `json:"job_status"`
-	Limit     int64  `json:"limit"`
-}
-
 // An ApiJobResponse represents a Job response.
 // Example response
 // {

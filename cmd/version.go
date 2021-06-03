1cda61f33e3b7f721d0c52a6a40a75a2b831aac8
Valeriy Soloviov <weldpua2008@gmail.com>
2021-06-03 22:25:22 +0300

    Update version.go

--- cmd/version.go
+++ cmd/version.go
@@ -22,7 +22,7 @@ var GitCommit string
 
 const (
 	// Version defines main version number
-	Version = "0.4.2"
+	Version = "0.4.3"
 
 	// VersionPrerelease is pre-release marker for the version
 	// such as "dev" (in development), "beta", "rc1", etc.
